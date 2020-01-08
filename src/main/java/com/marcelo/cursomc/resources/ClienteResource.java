90f1e073a39ffdc18ae3affe76f70d2c6b7a9049
Marcelo Oliveira <marcelolynx@gmail.com>
2020-01-08 11:23:00 -0400

    Iniciando as operações de CRUD para o model  Cliente

--- src/main/java/com/marcelo/cursomc/resources/ClienteResource.java
+++ src/main/java/com/marcelo/cursomc/resources/ClienteResource.java
@@ -1,13 +1,16 @@
 package com.marcelo.cursomc.resources;
 
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
+import com.marcelo.cursomc.domain.Categoria;
+import com.marcelo.cursomc.dto.CategoriaDTO;
+import com.marcelo.cursomc.dto.ClienteDTO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.marcelo.cursomc.domain.Cliente;
 import com.marcelo.cursomc.services.ClienteService;
@@ -26,6 +29,38 @@ public class ClienteResource {
         Optional<Cliente> obj = clienteService.find(id);
 
         return ResponseEntity.ok().body(obj);
+    }
+
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+    public ResponseEntity<?> delete(@PathVariable Integer id) {
+        clienteService.delete(id);
+
+        return ResponseEntity.noContent().build();
+
+    }
+
+    @RequestMapping(method = RequestMethod.GET)
+    public ResponseEntity<List<ClienteDTO>> findAll() {
+
+        List<Cliente>  list = clienteService.findAll();
+        List<ClienteDTO>
+                listDto = list.stream().map(obj -> new ClienteDTO(obj)).collect(Collectors.toList());
+
+        return ResponseEntity.ok().body(listDto);
+    }
+
+    @RequestMapping(value="/page",method = RequestMethod.GET)
+    public ResponseEntity<Page<ClienteDTO>> findPage(
+            @RequestParam(value="page", defaultValue="0")Integer page,
+            @RequestParam(value="linesPerPage", defaultValue="24")Integer linesPerPage,
+            @RequestParam(value="orderBy", defaultValue="nome")String orderBy,
+            @RequestParam(value="direction", defaultValue="ASC") String direction) {
+
+        Page<Cliente>  list = clienteService.findPage(page, linesPerPage, orderBy, direction);
+        Page<ClienteDTO> listDto = list.map(obj -> new ClienteDTO(obj));
+
+        return ResponseEntity.ok().body(listDto);
     }
 
 
--- src/main/java/com/marcelo/cursomc/services/ClienteService.java
+++ src/main/java/com/marcelo/cursomc/services/ClienteService.java
@@ -1,8 +1,18 @@
 package com.marcelo.cursomc.services;
 
+import java.util.List;
 import java.util.Optional;
 
+import com.marcelo.cursomc.domain.Categoria;
+import com.marcelo.cursomc.dto.CategoriaDTO;
+import com.marcelo.cursomc.dto.ClienteDTO;
+import com.marcelo.cursomc.exceptions.ObjectNotFoundException;
+import com.marcelo.cursomc.services.exceptions.DataIntegretyException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import com.marcelo.cursomc.domain.Cliente;
@@ -21,4 +31,46 @@ public class ClienteService {
 
     }
 
+    public Cliente update(Cliente obj) {
+        find(obj.getId());
+        if(obj.getId() == null) {
+            throw new ObjectNotFoundException("Objeto não encontrado! " + "Tipo: " + Categoria.class.getName());
+        }
+        return clienteRepository.save(obj);
+    }
+
+    public void delete(Integer id) {
+        find(id);
+        try {
+            clienteRepository.deleteById(id);
+
+        }
+        catch (DataIntegrityViolationException e) {
+            throw new DataIntegretyException("Não é possível deletar uma Categoria que tenha produtos relacionados");
+        }
+    }
+
+    public List<Cliente> findAll() {
+        return clienteRepository.findAll();
+
+    }
+
+    public Page<Cliente> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+        PageRequest pageRequest = PageRequest.of(page, linesPerPage, Sort.Direction.valueOf(direction), orderBy);
+        return clienteRepository.findAll(pageRequest);
+    }
+
+    public Cliente fromDTO(ClienteDTO objDto) {
+
+        return new Cliente(
+                objDto.getId(),
+                objDto.getNome(),
+                objDto.getEmail(),
+                null,
+                null
+        );
+    }
+
 }
+
+
