5cce7b08465ca57ff98e6d202ac593c8dc8e5c6f
Marcelo Oliveira <marcelolynx@gmail.com>
2020-01-10 09:44:21 -0400

    Ajustando e testando a deleção de Cliente

--- src/main/java/com/marcelo/cursomc/domain/Cliente.java
+++ src/main/java/com/marcelo/cursomc/domain/Cliente.java
@@ -6,13 +6,7 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
-import javax.persistence.CollectionTable;
-import javax.persistence.ElementCollection;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.OneToMany;
+import javax.persistence.*;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
@@ -46,7 +40,7 @@ public class Cliente implements Serializable {
     private List<Pedido> pedidos = new ArrayList<>();
 
     
-    @OneToMany(mappedBy = "cliente")
+    @OneToMany(mappedBy = "cliente", cascade = CascadeType.ALL)
     private List<Endereco> enderecos = new ArrayList<>();
 
     @ElementCollection
